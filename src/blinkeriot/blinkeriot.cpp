a080229bb092431edad21eb0cd00b860b370aec0
liuke <1250585541@qq.com>
2025-08-31 22:27:14 +0800

    feat: 点灯iot

--- src/blinkeriot/blinkeriot.cpp
+++ src/blinkeriot/blinkeriot.cpp
@@ -10,9 +10,9 @@
 
 #define LED_BUILTIN 15
 
-char auth[] = "b672ab1059dc";
+char auth[] = "xx";
 char ssid2[] = "201";
-char pswd2[] = "18162327131lk";
+char pswd2[] = "xx";
 
 BlinkerButton btn("btn");
 
